71569150bd46689326424469a4e8bf2eccfc320c
Naga Ganesh B J <bjnaga@gmail.com>
2024-04-25 13:15:44 +0530

    Add files via upload

---
+++ theory/u2-11-custom_transformers.py
@@ -0,0 +1,31 @@
+# Custom Transformers
+# create a class and implement fit(), transform(), fit_transform() methods
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+import os
+import pandas as pd
+
+file_path = os.path.join("datasets","housing","housing.csv")
+
+
+housing = pd.read_csv(file_path)
+
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator ,TransformerMixin):
+    def __init__(self,add_bedtooms_per_room = True):
+        self.add_bedtooms_per_room = add_bedtooms_per_room
+    def fit(self,X,y=None):
+        return self
+    def transform(self,X):
+        rooms_per_household = X[:,rooms_ix]/X[:,households_ix]
+        population_per_household = X[:,population_ix]/X[:,households_ix]
+        if self.add_bedtooms_per_room:
+            bedrooms_per_room = X[:,bedrooms_ix]/X[:,rooms_ix]
+            return np.c_[X,rooms_per_household,population_per_household,bedrooms_per_room]
+        else:
+            return np.c_[X,rooms_per_household,population_per_household]
+attr_adder = CombinedAttributesAdder(add_bedtooms_per_room= True)
+housing_extra_attribs = attr_adder.transform(housing.values)
+print(housing_extra_attribs.shape)
+print(housing_extra_attribs)
